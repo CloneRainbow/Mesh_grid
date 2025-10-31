8820c584d279715e23ac5337a2954e6458dd45a1
CloneRainbow <150621787+CloneRainbow@users.noreply.github.com>
2025-10-31 22:36:25 +0200

    Update export.py

--- utils/export.py
+++ utils/export.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def export_to_csv(df: pd.DataFrame, path: str):
+    df.to_csv(path, index=False)
+    return path
+
+def export_to_excel(df: pd.DataFrame, path: str):
+    df.to_excel(path, index=False)
+    return path
