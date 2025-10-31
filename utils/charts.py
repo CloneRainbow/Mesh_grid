3f2e2fb0ba089ba1849774c3889798bdc802613c
CloneRainbow <150621787+CloneRainbow@users.noreply.github.com>
2025-10-31 22:36:59 +0200

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,10 @@
+import plotly.express as px
+import pandas as pd
 
+def create_cost_chart(material, size, cost):
+    df = pd.DataFrame({"Матеріал": [material], "Вічко": [size], "Собівартість (грн)": [cost]})
+    return px.bar(df, x="Вічко", y="Собівартість (грн)", color="Матеріал", title="Собівартість")
+
+def create_weight_chart(height, weight):
+    df = pd.DataFrame({"Висота (м)": [height], "Вага (кг)": [weight]})
+    return px.line(df, x="Висота (м)", y="Вага (кг)", title="Вага vs Висота", markers=True)
