9c9de1e2737f6e30cdeaccc46d9f4ec964b6342b
CloneRainbow <150621787+CloneRainbow@users.noreply.github.com>
2025-11-01 13:38:04 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,14 @@
+import os
 
+DATA_DIR = "data"
+os.makedirs(DATA_DIR, exist_ok=True)
+
+MIN_STOCK = {
+    "Оцинкований": 50,
+    "Чорний": 30,
+    "ПВХ": 20
+}
+
+TRUCK_CAPACITY = 20000  # кг
+FUEL_CONSUMPTION = 0.35  # л/км
+CO2_PER_LITER = 2.3
